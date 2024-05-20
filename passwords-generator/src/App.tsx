72261ee79c93f077deceba06932730f50f179670
Andres2D <andres2d1997.1206@gmail.com>
2024-05-20 18:48:46 -0400

    feat: add basic html structure

--- passwords-generator/src/App.tsx
+++ passwords-generator/src/App.tsx
@@ -4,7 +4,19 @@ const App = () => {
 
   return (
     <>
-      <h1>App works!</h1>
+      <h1 className='app-title'>Password generator</h1>
+      <form className='form'>
+        <div className='password-input'>
+          <label 
+            htmlFor='password'
+            className='label'
+          >
+              Your new password
+          </label>
+          <input type='text' placeholder='andres123-' />
+        </div>
+        <button className='generate-button'>Generate</button>
+      </form>
     </>
   )
 }
