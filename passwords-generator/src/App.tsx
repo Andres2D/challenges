6698e9ba0d3fe32f8887fa023cc08de22b5f1caf
Andres2D <andres2d1997.1206@gmail.com>
2024-05-20 19:01:33 -0400

    feat: add basic input event handler

--- passwords-generator/src/App.tsx
+++ passwords-generator/src/App.tsx
@@ -1,21 +1,42 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
 
+  const [password, setPassword] = useState('');
+
+  const formHandler = (event: any) => {
+    event.preventDefault();
+    setPassword('ksdjfkjwe786823423-.,g')
+  }
+
   return (
     <>
       <h1 className='app-title'>Password generator</h1>
-      <form className='form'>
+      <form className='form' onSubmit={formHandler}>
         <div className='password-input'>
           <label 
             htmlFor='password'
             className='label'
           >
-              Your new password
+            Your new password
           </label>
-          <input type='text' placeholder='andres123-' />
+          <div>
+            <input 
+              type='text'
+              value={password}
+              placeholder='sdkfjhsdf4r23-'
+              disabled
+            />
+            <button>copy</button>
+          </div>
         </div>
-        <button className='generate-button'>Generate</button>
+        <button 
+          className='generate-button'
+          type='submit'
+        >
+          Generate
+        </button>
       </form>
     </>
   )
